a54e091803dcaf239f58353815cab416eebd126d
dheerajreddymumma <dheerajreddymumma@gmail.com>
2019-10-09 16:35:36 -0400

    objective 1 of lab 7

---
+++ src/edu/osu/cse5234/util/ServiceLocator.java
@@ -0,0 +1,14 @@
+package edu.osu.cse5234.util;
+import edu.osu.cse5234.business.OrderProcessingServiceBean;
+import javax.naming.*;
+
+public class ServiceLocator {
+	public static OrderProcessingServiceBean getOrderProcessingService() {
+		try {
+	         return (OrderProcessingServiceBean) InitialContext.doLookup(
+					"java:module/OrderProcessingServiceBean!edu.osu.cse5234.business.OrderProcessingServiceBean");
+		} catch (NamingException ne) {
+				throw new RuntimeException(ne);
+		}
+	}
+}
